660145f277992531ab8c9053596115bb8a87a1ca
Dannael Paz <dannaelpaz@gmail.com>
2021-12-04 09:32:22 -0300

    final da parte react

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const { json } = require('express');
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { v4, validate } = require('uuid');
+const { isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -8,16 +9,48 @@ app.use(json());
 
 const projects = [];
 
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+  console.time(logLabel);
+
+  next();
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID' });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
+
 // rota que lista os projetos na memória
 app.get('/projects', (request, response) => {
-  return response.json(projects);
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
 });
 
 // rota que adiciona um novo projeto na lista
 app.post('/projects/', (request, response) => {
   const { title, owner } = request.body;
 
-  const project = { id: uuid(), title, owner };
+  const project = { id: v4(), title, owner };
 
   projects.push(project);
 
